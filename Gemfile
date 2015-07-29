7528fb534a3f0a1bddd1741feb465b54578863ad
pekermert <pekermert@email.com>
2015-07-29 17:58:52 +0300

    ipa file parser included

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'apktools'
-gem 'rubyzip',  "~> 0.9.9"
+gem 'rubyzip', '~>1.0.0'
 gem 'pry'
+gem 'ipa_reader'
--- apk-parser.rb
+++ apk-parser.rb
@@ -1,44 +1,51 @@
 require 'apktools/apkxml'
 require 'pry'
+require 'ipa_reader'
+
+class Parser
+	class << self
+		def apk_parser(file)
+			parser = ApkXml.new(file)
+			parser.parse_xml("AndroidManifest.xml", false, true)
+
+			elements = parser.xml_elements
+
+			res = {}
+
+			%w(manifest activity application uses-sdk).each do |attrb|
+				element = elements.select {|element| element.name == attrb}.first
+				res[attrb] = element.attributes.map { |attribute| { attribute.name => attribute.value } }
+			end
+
+			puts res
+		end
+
+		def ipa_parser(file)
+			ipa = IpaReader::IpaFile.new("butcele.ipa")
+			#name target_os_version minimum_os_version url_schemes bundle_identifier icon_prerendered
+			#ipa.instance_variables.each {|var| hash[var.to_s.delete("@")] = ipa.instance_variable_get(var) }
+			hash = {'name' => ipa.name, 
+					'target_os' => ipa.target_os_version,
+					'min_os' => ipa.minimum_os_version,
+					'bundle_identifier' => ipa.bundle_identifier,
+					'icon' => ipa.icon_prerendered,
+					'url_schemes' => ipa.url_schemes
+				}
+				
+			p hash
+		end
+	end
+end
+
 if ARGV.length != 1
 	puts "usage: get_app_version <APKFile>"
 	exit(1)
 end
 
-apk_file = ARGV[0]
-
-parser = ApkXml.new(apk_file)
-parser.parse_xml("AndroidManifest.xml", false, true)
-
-elements = parser.xml_elements
-
-attribute1 = nil
-attribute2 = nil
-attribute3 = nil
-attribute4 = nil
-
-elements.each do |element|
-	if element.name == 'manifest'
-		attribute1 = element.attributes.map do |attribute|
-			{attribute.name => attribute.value}
-		end
-	elsif element.name == 'activity'
-		attribute2 = element.attributes.map do |attribute|
-			{attribute.name => attribute.value}
-		end
-	elsif element.name == 'application'
-		attribute3 = element.attributes.map do |attribute|
-			{attribute.name => attribute.value}
-		end
-	elsif element.name == 'uses-sdk'
-		attribute4 = element.attributes.map do |attribute|
-			{attribute.name => attribute.value}
-		end
-	end
-
+file = ARGV[0]
+unless file =~ /(.ipa|.apk)$/
+	puts 'Wrong type of file'
+	exit(1)
 end
-
-puts attribute1
-puts attribute2
-puts attribute3
-puts attribute4
+meth = file =~ /.ipa$/ ? 'ipa_parser' : 'apk_parser'
+Parser.send(meth, file)
